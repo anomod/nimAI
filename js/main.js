5bfb7926c0118ea5bb4c8cd856caf5de729025e7
Thomas van Egmond <t.m.vanegmond@students.uu.nl>
2023-01-10 21:53:40 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let p5 = require("p5");
+
+class Field {
+    field;
+    constructor() {
+        field = GeneratePiles(Math.random(2,21), Math.random(1,10));
+    }
+
+    GeneratePiles(numPiles, maxBlocks) {
+        let piles = [];
+        for (let i = 0; i < numPiles; i++) {
+          piles.push(Math.floor(Math.random() * (maxBlocks + 1)));
+        }
+        return piles;
+      }
+
+    DrawPiles(field) {
+        for(let i = 0; i < field.length; i++) {
+            for(let j = 0; j < field[i]; j++) {
+                
+            }
+        }
+    }
+}
+
+class Block {
+    x;
+    y;
+    width;
+    height;
+    constructor(x, y) {
+
+        
+    }
+
+    Update() {
+
+    }
+ 
+    Draw() {
+        rectangle(x,y,widht,height);
+    }
+}
